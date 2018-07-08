d0aeb5051019476c759ad230523963b79882e35e
Joe Wegner <joe@wegnerdesign.com>
2018-07-07 19:50:50 -0500

    basic webpack configuration

--- src/server/index.js
+++ src/server/index.js
@@ -4,9 +4,10 @@ const express = require('express')
 let app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+app.use('/assets', express.static('dist'))
 
 app.get('/', function(req, res) {
-  res.render('index')
+  res.render('index', { title: 'Home' })
 })
 
 app.listen(3000)
