6091bfe43b23178507d04f4f1053e52a7f4a771b
Joe Wegner <joe@wegnerdesign.com>
2018-07-09 16:29:33 -0500

    add mailchimp form

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -4,3 +4,18 @@ window.addEventListener('scroll', function () {
   var pcnt = document.body.scrollTop / document.body.scrollHeight * 3;
   document.querySelectorAll('.bg-beans')[0].style = "background-position-y: "+(pcnt * 100)+"%";
 });
+
+var footerNewsletter = document.querySelectorAll('.footer-newsletter')
+if (footerNewsletter.length) {
+  footerNewsletter[0].addEventListener('submit', function() {
+    setTimeout(function() {
+      var success = document.createElement('h4')
+      success.className = 'h2 text-center text-success my-5'
+      success.innerText = "Thanks! We'll let you know when there's coffee available!"
+
+      footerNewsletter[0].replaceWith(success)
+    })
+
+    return true
+  })
+}
